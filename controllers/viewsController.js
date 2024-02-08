752cf0faf9fa07266acf3925b9c4c7c5b84154fd
TK <t.koprivnik@gmail.com>
2024-02-08 19:01:40 +0100

    Add view controller

---
+++ controllers/viewsController.js
@@ -0,0 +1,30 @@
+const Tour = require('../models/tourModel');
+
+exports.getOverview = async (req, res, next) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).render('overview', {
+      title: 'All Tours',
+      tours,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getTour = async (req, res, next) => {
+  try {
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+      path: 'reviews',
+      fields: 'review rating user',
+    });
+
+    res.status(200).render('tour', {
+      title: 'Tour',
+      tour,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
