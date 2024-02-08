48d04ef00c4fb13d6aa44bfd93d0f7e5857637f8
TK <t.koprivnik@gmail.com>
2024-02-08 19:01:17 +0100

    Add view router

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -86,9 +87,7 @@ app.use((req, res, next) => {
 });
 
 // 2) Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base');
-});
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
